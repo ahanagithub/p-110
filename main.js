eb9f367d5e69cf9ceb41ec7cf01f95ad9bdddd30
ahanagithub <83067851+ahanagithub@users.noreply.github.com>
2022-04-18 12:58:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-//https://teachablemachine.withgoogle.com/models/6B0SwFHDR/
+prediction_1="Ahana is ";
+prediction_2="studying going in 7th standard";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    })
+    speak();
+}
+
+console.log('ml5 version is', ml5.version)
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/6B0SwFHDR/model.json', modelLoaded)
+
+function modelLoaded()
+{
+    console.log('model is loaded')
+}
+
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speak_data_1="1st prediction is "+prediction_1;
+    speak_data_2="and 2nd prediction is "+prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+}
